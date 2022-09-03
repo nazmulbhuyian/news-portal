92b0d8ded62f8b539704b18b99e71546f54df57c
unknown <nazmulnobel000@gmail.com>
2022-09-03 11:13:31 +0600

    spinner done

--- js/style.js
+++ js/style.js
@@ -5,41 +5,59 @@ const loadEntertainment = async() => {
     const res = await fetch(url)
     const data = await res.json()
     displayEntertainment(data.data)
+    toggleSpiner(true);
 }
 
-const displayEntertainment = (countries) => {
+const displayEntertainment = (entertainments) => {
+    toggleSpiner(true);
+    
+    const containerEntertainment = document.getElementById('entertainments-container');
+    entertainments.forEach(entertainment => {
+        const entertainmentDiv = document.createElement('div');
+        entertainmentDiv.classList.add('entertainment');
 
-    const countryContainer = document.getElementById('countries-container');
-    countries.forEach(country => {
-        const countryDiv = document.createElement('div');
-        countryDiv.classList.add('country');
-
-        countryDiv.innerHTML = `
+        entertainmentDiv.innerHTML = `
             
             <div class="card mb-3">
                 <div class="row g-0">
                   <div class="col-md-4">
-                    <img src="${country?.image_url}" class="img-fluid rounded-start" alt=""  width="700px">
+                    <img src="${entertainment?.image_url}" class="img-fluid rounded-start" alt=""  width="700px">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
-                      <h5 class="card-title">${country.title}</h5>
-                      <p class="card-text">${country.details}</p>
+                      <h5 class="card-title">${entertainment.title}</h5>
+                      <p class="card-text">${entertainment.details}</p>
                       <div class="d-flex  align-items-center">
                       <div class="d-flex me-5 justify-content-center align-items-center">
-                      <img src= ${country.author.img} class="img-thumbnail me-2" alt="" width="80px">
-                      <h5>Name: ${country.author.name}</h5>
+                      <img src= ${entertainment.author.img} class="img-thumbnail me-2" alt="" width="80px">
+                      <h5>Name: ${entertainment.author.name}</h5>
                       </div>
-                      ${country.total_view == 0? "<button class='viewPop'><i class='fa-sharp fa-solid fa-eye'></i>No View</button>": country.total_view}
-                      <button class="btn btn-primary ms-5 mx-4 my-2">Details</button>
+                      ${entertainment.total_view == 0? "<button class='viewPop'><i class='fa-sharp fa-solid fa-eye'></i>No View</button>": entertainment.total_view}
+                      <button id="details-btn" class="btn btn-primary ms-5 mx-4 my-2">Details</button>
                       </div>
                     </div>
                   </div>
                 </div>
             </div>
         `
-        countryContainer.appendChild(countryDiv);
-
+        containerEntertainment.appendChild(entertainmentDiv);
+        
 
     })
+    toggleSpiner(false);
 }
+
+const toggleSpiner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading === true){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+document.getElementById('details-btn').addEventListener('click', function(){
+    alert ('how are you')
+})
